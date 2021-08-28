38ea8a39d825e6ec528237b8fb0b0209eee7f10b
chyroc <chyroc@qq.com>
2021-08-28 12:10:02 +0800

    git commit

--- commiter/go.mod
+++ commiter/go.mod
@@ -1,3 +1,5 @@
 module github.com/chyroc/action.sh/commiter
 
 go 1.17
+
+require github.com/chyroc/goexec v0.1.0
--- commiter/main.go
+++ commiter/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"strings"
+
+	"github.com/chyroc/goexec"
 )
 
 func main() {
 	fmt.Println("action.sh::commiter")
+
+	addFiles := flag.String("add", "", "add files, split by `,`")
+	msg := flag.String("msg", "", "msg")
+
+	flag.Parse()
+
+	setupActionUser()
+
+	gitAddFiles(strings.Split(*addFiles, ","))
+	gitCommit(*msg)
+
 }
+
+// by: https://github.community/t/github-actions-bot-email-address/17204/5
+func setupActionUser() {
+	// goexec.New("git","config","--global","user.name","").RunInStream()
+	assert(goexec.New("git", "config", "--global", "user.email", "41898282+github-actions[bot]@users.noreply.github.com").RunInStream())
+}
+
+func gitAddFiles(files []string) {
+	for _, file := range files {
+		file = strings.TrimSpace(file)
+		_ = goexec.New("git", "add", file).RunInStream()
+	}
+}
+
+func gitCommit(msg string) {
+	_ = goexec.New("git", "commit", "-a", "-m", msg).RunInStream()
+}
+
+func assert(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// git commit -am "commit-by-action: $(date)" || (echo "no commit" && exit 0)
+// git push
